1ecc9ed8333a6872a78546d682527e346c293abc
David Burian <david.burian@me.com>
2019-12-14 20:00:15 +0000

    Validation for architects done

--- server/app/models/architect.rb
+++ server/app/models/architect.rb
@@ -1,2 +1,18 @@
 class Architect < ApplicationRecord
+  validates :name, presence: true,
+                   uniqueness: true, length: { maximum: 50 }
+
+  POSITION_OPTIONS = %w(project_architect leading_architect)
+  validates :position, presence: true,
+                       inclusion: { in: POSITION_OPTIONS,
+                                    message: "%{value} is one of #{POSITION_OPTIONS}"
+                                  }
+    
+  validates :desc, presence: true,
+                   if: :is_leading_architect?
+
+  def is_leading_architect?
+   logger.debug position
+   position == "leading_architect"
+  end
 end
