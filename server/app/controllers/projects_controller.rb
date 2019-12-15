329dea0e6ef694a64f464f49261f8b627f3b770c
David Burian <david.burian@me.com>
2019-12-15 23:29:12 +0000

    Projects admin now works

--- server/app/controllers/projects_controller.rb
+++ server/app/controllers/projects_controller.rb
@@ -24,16 +24,7 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    pp = permitted_params
-    asoc_p = association_attr pp
-    @project = Project.new(name: pp[:name],
-                           long_desc: pp[:long_desc],
-                           short_desc: pp[:short_desc],
-                           year: pp[:year],
-                           function_id: pp[:function_id],
-                           status_id: pp[:status_id],
-                           project_architect_associations_attributes: asoc_p
-                          )
+    @project = Project.new(project_params)
 
     #architect_ids.map { |id| @project.architects << Architect.find(id) }
 
@@ -79,7 +70,7 @@ class ProjectsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def permitted_params
+    def project_params
       base = params.require(:project).permit(
           :name,
           :long_desc,
@@ -92,14 +83,4 @@ class ProjectsController < ApplicationController
       logger.debug base
       return base
     end
-    def association_attr pp
-      arch_ids = pp[:architect_ids].select {
-        |id| !id.blank?
-      }
-      logger.debug arch_ids
-      ret = arch_ids.map { |id| { architect_id: id } }
-      logger.debug ret
-      return ret
-    end
-      
 end
