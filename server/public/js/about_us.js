e245ea9572ed88b81460dac4289b9408998ba2f4
fthamm <ft.hamm@gmx.de>
2019-12-16 15:45:18 +0100

    get about_us from json

--- server/public/js/about_us.js
+++ server/public/js/about_us.js
@@ -1,14 +1,56 @@
-$(document).ready(function(){
-
-    $("#architects_list").empty();
-    $.getJSON("json/projects.json","",function(json){
-        $.each(json,function () {
-            $.each(this.architects, function(){
-                $('#architects_list').append(
-                    $('<h3/>')
-                        .text(this)
-                );
-            })
-        })
+$(document).ready(function() {
+  $("#architects_list").empty();
+  $.getJSON("json/projects.json", "", function(json) {
+    $.each(json, function() {
+      $.each(this.architects, function() {
+        $("#architects_list").append($("<h3/>").text(this));
+      });
     });
+  });
+
+  $("#description_grid").empty();
+  $.getJSON("json/about_us.json", "", function(json) {
+    $("#description_grid").append($("<p/>").text(json.description));
+    $("#description_grid").append($("<p/>").text(json.description));
+  });
+
+  $("#leading_architects_article").empty();
+  $.getJSON("json/about_us.json", "", function(json) {
+    $.each(json.leading_architects, function() {
+      $("#leading_architects_article").append(
+        $("<div/>")
+          .append(
+            $("<div/>")
+              .addClass("leader-arch-thumb-wrapper")
+              .append(
+                $("<div/>")
+                  .addClass("square-thumb")
+                  .append(
+                    $("<div/>")
+                      .addClass("square-shape")
+                      .append(
+                        $("<div/>")
+                          .addClass("max-volume")
+                          .append(
+                            $("<img/>")
+                              .addClass("thumb-image")
+                              .attr("src", "images/" + this.img)
+                          )
+                      )
+                  )
+                  .append(
+                    $("<h3>")
+                      .addClass("square-thumb-header")
+                      .text(this.name)
+                  )
+              )
+          )
+          .append(
+            $("<p/>")
+              .addClass("leader-arch-desc")
+              .text(this.description)
+          )
+      );
+    });
+  });
 });
