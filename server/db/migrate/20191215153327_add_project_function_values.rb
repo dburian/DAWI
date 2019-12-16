1fa75726491cf2a09ef694000109792c447dc8c9
David Burian <david.burian@me.com>
2019-12-16 12:28:04 +0000

    Add admin model, seeding now from db:seed

--- server/db/migrate/20191215153327_add_project_function_values.rb
+++
@@ -1,8 +0,0 @@
-class AddProjectFunctionValues < ActiveRecord::Migration[6.0]
-  def change
-    ProjectFunction.create name: "Public Building"
-    ProjectFunction.create name: "Residential Building"
-    ProjectFunction.create name: "Administration Building"
-    ProjectFunction.create name: "Urbanism"
-  end
-end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_110346) do
+ActiveRecord::Schema.define(version: 2019_12_16_115609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_12_16_110346) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "architect_positions", force: :cascade do |t|
