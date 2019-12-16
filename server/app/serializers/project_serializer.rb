9a08ce18e70da8b0a881500302f7fe23e04bfdc5
fthamm <ft.hamm@gmx.de>
2019-12-16 22:01:45 +0100

    stuff

--- server/app/serializers/project_serializer.rb
+++ server/app/serializers/project_serializer.rb
@@ -2,16 +2,17 @@ class ProjectSerializer < ActiveModel::Serializer
 
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :long_desc, :year, :function_id, :status_id, :images, :architects
+  attributes :id, :name, :long_desc, :year, :function_id, :status_id, :architects, :images
 
-  def images
-    return unless object.images.attachments
-    image_urls = object.images.map do |image| 
-      URI.join(
-        ActionController::Base.asset_host, 
-        rails_blob_path(image))
-    end
+ #def images
+ # return unless object.images.attachments
+ # image_urls = object.images.map do |image| 
+ #   URI.join(
+ #     ActionController::Base.asset_host, 
+ #     rails_blob_path(image))
+ # end
+ #
+ # image_urls
+ #end 
 
-    image_urls
-  end
 end
