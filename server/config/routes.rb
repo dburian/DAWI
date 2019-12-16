78cd0297d57c051e12d327c486b0d0b4e3a1bf2c
David Burian <david.burian@me.com>
2019-12-16 17:10:24 +0000

    Admin page done

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -13,4 +13,16 @@ Rails.application.routes.draw do
     get '*unmatched_route', to: 'sessions#new'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'pages#index'
+  get "index" => "pages#index"
+  get "project_detail" => "pages#project_detail"
+  get "ordered_projects" => "pages#ordered_projects"
+  get "contacts" => "pages#contacts"
+  get "about_us" => "pages#about_us"
+  
+  get '*unmatched_route', to: 'pages#index'
+  # Mounting api shoudl be somewhere else (like /api)
+  # in root we have client pages
+  #mount API::Base, at: "/"
 end
