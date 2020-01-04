9ccc903cb19ea8533d7461ad5322e0b5f86f94d9
David Burian <david.burian@me.com>
2020-01-04 17:06:00 +0000

    Scss in webpack, custom fonts working

--- server/app/assets/config/manifest.js
+++ server/app/assets/config/manifest.js
@@ -1,3 +1,2 @@
-//= link_directory ../fonts
 //= link_tree ../images
 //= link_directory ../stylesheets .css
--- server/config/webpack/environment.js
+++ server/config/webpack/environment.js
@@ -1,5 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+// Get the actual sass-loader config
+const sassLoader = environment.loaders.get('sass');
+const sassLoaderConfig = sassLoader.use.find(function(element) {
+  return element.loader == 'sass-loader';
+})
+
+// Use Dart-implementation of Sass (default is node-sass)
+const options = sassLoaderConfig.options;
+options.implementation = require('sass');
+
 const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
